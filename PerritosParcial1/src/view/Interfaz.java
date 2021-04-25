a8981a4ff674f333803aa1070f27939885f89c52
Laura Cobo <53491875+moonlao@users.noreply.github.com>
2021-04-25 18:12:27 -0500

    Add files via upload

--- PerritosParcial1/src/view/Interfaz.java
+++ PerritosParcial1/src/view/Interfaz.java
@@ -23,42 +23,47 @@ public class Interfaz {
 		if (a.get(0).getNombre().equals("Martina")) {
 			PImage perfiles = app.loadImage("./data/imports/" + a.get(0).getNombre() + ".jpeg");
 			app.image(perfiles, 20, 20, 100, 100);
-			app.text(a.get(0).getNombre(), 30, 220);
-			app.text(a.get(0).getRaza(), 30, 240);
-			app.text(a.get(0).getFecha(), 30, 280);
-			app.text(a.get(0).getId(), 30, 300);
+			app.textSize(15);
+			app.text(a.get(0).getNombre(), 20, 220);
+			app.text(a.get(0).getRaza(), 20, 240);
+			app.text(a.get(0).getFecha(), 20, 280);
+			app.text(a.get(0).getId(), 20, 300);
 		}
 		if (a.get(1).getNombre().equals("Kaiser")) {
 			PImage perfiles = app.loadImage("./data/imports/" + a.get(1).getNombre() + ".jpeg");
-			app.image(perfiles, 120, 20, 100, 100);
-			app.text(a.get(1).getNombre(), 120, 220);
-			app.text(a.get(1).getRaza(), 120, 240);
-			app.text(a.get(1).getFecha(), 120, 280);
-			app.text(a.get(1).getId(), 120, 300);
+			app.image(perfiles, 150, 20, 100, 100);
+			app.textSize(15);
+			app.text(a.get(1).getNombre(), 160, 220);
+			app.text(a.get(1).getRaza(), 160, 240);
+			app.text(a.get(1).getFecha(), 160, 280);
+			app.text(a.get(1).getId(), 160, 300);
 		}
 		if (a.get(2).getNombre().equals("Heisenberg")) {
 			PImage perfiles = app.loadImage("./data/imports/" + a.get(2).getNombre() + ".jpeg");
-			app.image(perfiles, 220, 20, 100, 100);
-			app.text(a.get(2).getNombre(), 220, 220);
-			app.text(a.get(2).getRaza(), 220, 240);
-			app.text(a.get(2).getFecha(), 220, 280);
-			app.text(a.get(2).getId(), 220, 300);
+			app.image(perfiles, 280, 20, 100, 100);
+			app.textSize(15);
+			app.text(a.get(2).getNombre(), 300, 220);
+			app.text(a.get(2).getRaza(), 300, 240);
+			app.text(a.get(2).getFecha(), 300, 280);
+			app.text(a.get(2).getId(), 300, 300);
 		}
 		if (a.get(3).getNombre().equals("Lola")) {
 			PImage perfiles = app.loadImage("./data/imports/" + a.get(3).getNombre() + ".jpeg");
-			app.image(perfiles, 320, 20, 100, 100);
-			app.text(a.get(3).getNombre(), 320, 220);
-			app.text(a.get(3).getRaza(), 320, 240);
-			app.text(a.get(3).getFecha(), 320, 280);
-			app.text(a.get(3).getId(), 320, 300);
+			app.image(perfiles, 410, 20, 100, 100);
+			app.textSize(15);
+			app.text(a.get(3).getNombre(), 440, 220);
+			app.text(a.get(3).getRaza(), 440, 240);
+			app.text(a.get(3).getFecha(), 440, 280);
+			app.text(a.get(3).getId(), 440, 300);
 		}
 		if (a.get(4).getNombre().equals("Nymeria")) {
 			PImage perfiles = app.loadImage("./data/imports/" + a.get(4).getNombre() + ".jpeg");
-			app.image(perfiles, 420, 20, 100, 100);
-			app.text(a.get(4).getNombre(), 420, 220);
-			app.text(a.get(4).getRaza(), 420, 240);
-			app.text(a.get(4).getFecha(), 420, 280);
-			app.text(a.get(4).getId(), 420, 300);
+			app.image(perfiles, 540, 20, 100, 100);
+			app.textSize(15);
+			app.text(a.get(4).getNombre(), 580, 220);
+			app.text(a.get(4).getRaza(), 580, 240);
+			app.text(a.get(4).getFecha(), 580, 280);
+			app.text(a.get(4).getId(), 580, 300);
 
 		}
 	}
